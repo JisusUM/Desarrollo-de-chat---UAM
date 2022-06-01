e19e596793adadf8435bbb07dd30e6b016e2057b
JisusUM <jhlopez94173@umanizales.edu.co>
2022-06-01 08:45:08 -0500

    3 version

--- client.py
+++ client.py
@@ -61,49 +61,51 @@ def send_message():
     else:
         messagebox.showerror("mensaje vacio", "El mensaje no puede estar vacío")
 
-root = tk.Tk()
+root = Tk()
 root.geometry("600x600")
-#logo=tk.PhotoImage(file="code.gif")
-"""image= image.subsample(3,3)
-label=tk.Label(image=image)
-label.pack()"""
 root.title("Soporte Centro de informatica")
 root.resizable(False, False)
-logo=tk.PhotoImage(file="code.gif")
+bg =PhotoImage(file="code.gif")
+
+# Show image using label
+label1 = Label( root, image = bg)
+label1.place(x = 0, y = 0)
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=4)
 root.grid_rowconfigure(2, weight=1)
 
-top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
-top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+top_frame = Frame(root, width=600, height=100, bg=DARK_GREY)
+top_frame.grid(row=0, column=0, sticky=NSEW)
 
-middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
-middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+middle_frame = Frame(root, width=600, height=400, bg=MEDIUM_GREY)
+middle_frame.grid(row=1, column=0, sticky=NSEW)
 
-bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
-bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+bottom_frame = Frame(root, width=600, height=100, bg=DARK_GREY)
+bottom_frame.grid(row=2, column=0, sticky=NSEW)
 
-username_label = tk.Label(top_frame, text="Nombre de usuario:", font=FONT, bg=DARK_GREY, fg=WHITE)
-username_label.pack(side=tk.LEFT, padx=4)
+username_label = Label(top_frame, text="Nombre de usuario:", font=FONT, bg=DARK_GREY, fg=WHITE)
+username_label.pack(side=LEFT, padx=4)
 
-username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=23)
-username_textbox.pack(side=tk.LEFT)
+username_textbox = Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=23)
+username_textbox.pack(side=LEFT)
 
-username_button = tk.Button(top_frame, text="Unirse", font=BUTTON_FONT, bg=AZUL, fg=WHITE, command=connect)
-username_button.pack(side=tk.LEFT, padx=15)
+username_button = Button(top_frame, text="Unirse", font=BUTTON_FONT, bg=AZUL, fg=WHITE, command=connect)
+username_button.pack(side=LEFT, padx=15)
 
-message_textbox = tk.Entry(bottom_frame, font=FONT, bg=AZUL, fg=WHITE, width=38)
-message_textbox.pack(side=tk.LEFT, padx=10)
+message_textbox = Entry(bottom_frame, font=FONT, bg=AZUL, fg=WHITE, width=38)
+message_textbox.pack(side=LEFT, padx=10)
 
-message_button = tk.Button(bottom_frame, text="Enviar", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
-message_button.pack(side=tk.LEFT, padx=10)
+message_button = Button(bottom_frame, text="Enviar", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
+message_button.pack(side=LEFT, padx=10)
 
 
-message_box =scrolledtext.ScrolledText(middle_frame,font=SMALL_FONT,bg=None, fg=WHITE, width=67, height=26.5)
-message_box=tk.Label(middle_frame,image=logo)
-message_box.config(state=tk.DISABLED)
-message_box.pack(side=tk.TOP)
+message_box =scrolledtext.ScrolledText(middle_frame,font=SMALL_FONT,bg=AZUL, fg=WHITE, width=67, height=26.5)
+message_box.config(state=DISABLED)
+message_box.pack(side=TOP)
+
+
+
 
 
 def listen_for_messages_from_server(client):
